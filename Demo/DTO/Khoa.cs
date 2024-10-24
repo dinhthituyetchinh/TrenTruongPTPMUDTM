a12418c0f5e6d7f35bfa14a9d42d440c0c20531c
Tuyet Chinh <fengdinh@gmail.com>
2024-10-24 13:33:59 +0700

    b11

---
+++ Demo/DTO/Khoa.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+  public  class Khoa
+    {
+
+        string _MaKhoa;
+
+        public string MaKhoa
+        {
+            get { return _MaKhoa; }
+            set { _MaKhoa = value; }
+        }
+        string _TenKhoa;
+
+        public string TenKhoa
+        {
+            get { return _TenKhoa; }
+            set { _TenKhoa = value; }
+        }
+    }
+}
---
+++ Demo/FB/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DTO;
+using DAL_FB;
+namespace FB
+{
+    public partial class Form1 : Form
+    {
+       
+        public Form1()
+        {
+            InitializeComponent();
+            this.Load += Form1_Load;
+        }
+        public async void LoadKhoa()
+        {
+            // Load du lieu
+            List<Khoa> dskhoa = await DAL_Khoa_FB.GetThongTinKhoa("Khoa");
+
+            comboBox1.DataSource = dskhoa;
+            comboBox1.DisplayMember = "TenKhoa";
+        }
+        void Form1_Load(object sender, EventArgs e)
+        {
+            LoadKhoa();
+        }
+    }
+}
